f6f3ac819ba91f8a6865cd13607269967d2640a6
pzistart <pzistart@pzi.com>
2021-11-30 20:49:58 +0800

    third-commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 
 }
