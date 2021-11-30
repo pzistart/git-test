9f63f55b80a52a992937e10909bdc76e07e50226
pzistart <pzistart@pzi.com>
2021-11-30 21:18:34 +0800

    master-commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master-commit");
         System.out.println("hot-fix-commit2");
+        System.out.println("master-commit-2");
     }
 
 }
