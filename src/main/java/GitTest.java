8020ef918ef76d8c45d8d46b1503539fb6e031da
pzistart <pzistart@pzi.com>
2021-11-30 21:13:53 +0800

    hot-fix-commit1

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("git test2");
         System.out.println("git test3");
+        System.out.println("hot-fix");
     }
 
 }
